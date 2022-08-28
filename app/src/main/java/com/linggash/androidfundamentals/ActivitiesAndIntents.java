24f0e4dff08d4b1d0b32cc33fb269da4419124c8
Lingga Sempana Hidayat <sempanalingga@gmail.com>
2022-08-28 11:08:30 +0700

    Activitiy and Intents finished

--- app/src/main/java/com/linggash/androidfundamentals/ActivitiesAndIntents.java
+++ app/src/main/java/com/linggash/androidfundamentals/ActivitiesAndIntents.java
@@ -7,9 +7,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivitiesAndIntents extends AppCompatActivity {
 
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
     public static final String EXTRA_MESSAGE = "com.linggash.androidfundamentals.extra.MESSAGE";
     private EditText mMessageEditText;
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -19,14 +23,30 @@ public class ActivitiesAndIntents extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activities_and_intents);
         mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK){
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 
     public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
         String message = mMessageEditText.getText().toString();
-
-        startActivity(intent);
         intent.putExtra(EXTRA_MESSAGE, message);
-        Log.d(LOG_TAG, "Button clicked!");
+        startActivityForResult(intent, TEXT_REQUEST);
     }
 }
